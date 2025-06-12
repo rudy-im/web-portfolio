a8924c05c3d3135962daaabb374bace719914707
Rudy Im <141587105+rudy-im@users.noreply.github.com>
2025-06-11 20:47:00 -0400

    Create script.js

---
+++ Markdown Previewer/script.js
@@ -0,0 +1,53 @@
+import React from "https://esm.sh/react";
+import ReactDOM from "https://esm.sh/react-dom";
+import redux from "https://esm.sh/redux";
+import reactRedux from "https://esm.sh/react-redux";
+
+
+
+
+const SET_CONTENT = 'SET_CONTENT';
+
+const setContent = (text) => ({
+  type: SET_CONTENT,
+  payload: text
+});
+
+
+const reducer = (state = { content: '' }, action) => {
+  switch (action.type) {
+    case SET_CONTENT:
+      return { ...state, content: action.payload };
+    default:
+      return state;
+  }
+};
+
+
+const store = Redux.createStore(reducer);
+const { Provider, useDispatch, useSelector } = ReactRedux;
+
+function App() {
+  const content = useSelector((state) => state.content);
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    dispatch(setContent(e.target.value));
+  };
+
+  return (
+    <div className="container">
+      <div className="viewer" id="preview">{content}</div>
+      <div className="editor">
+        <textarea id="editor" value={content} onChange={handleChange} />
+      </div>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
