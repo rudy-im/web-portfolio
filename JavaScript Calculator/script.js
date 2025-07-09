e7d2f893dc7aea1fe4424bedef2dbc2466ec6c0a
Rudy Im <141587105+rudy-im@users.noreply.github.com>
2025-07-09 16:08:37 -0400

    Update script.js

--- JavaScript Calculator/script.js
+++ JavaScript Calculator/script.js
@@ -1,1 +1,23 @@
 
+$(document).ready(function() {
+  let display = $("#display");
+
+  $(".btn").not("#equals, #clear").click(function() {
+    let current = display.val();
+    let value = $(this).text();
+    display.val(current + value);
+  });
+
+  $("#clear").click(function() {
+    display.val("");
+  });
+
+  $("#equals").click(function() {
+    try {
+      let result = math.evaluate(display.val());
+      display.val(result);
+    } catch {
+      display.val("Error");
+    }
+  });
+});
